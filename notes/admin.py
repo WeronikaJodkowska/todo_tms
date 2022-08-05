921a2074d7c309d77ce9f1b837745e466778c247
Weronika Jodkowska <veronica.jodkowska@gmail.com>
2022-08-05 10:36:12 -0700

    Lesson 23 homework added

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from notes.models import Note
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "created_at")
+    fields = ("author", "title", "text")
+    search_fields = ("title", "text")
+    raw_id_fields = ("author", )
--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import json
+import requests
 
-# Create your tests here.
+from faker import Faker
+
+fake = Faker()
+
+
+if __name__ == "__main__":
+    response = requests.get("http://127.0.0.1:8000/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Index page"}
+    assert response.headers.get("content-type") == "application/json"
+
+    headers = {"Content-Type": "application/json"}
+    data = {"email": "", "password": ""}
+    response = requests.post("http://127.0.0.1:8000/user/", data=json.dumps(data), headers=headers)
+    assert response.status_code == 400
+
+    headers = {"Content-Type": "application/json"}
+    data = {"email": fake.email(), "password": fake.word()}
+    response = requests.post("http://127.0.0.1:8000/user/", data=json.dumps(data), headers=headers)
+
+    assert response.status_code == 201
+    assert response.json().get("email") == data.get("email")
+    assert response.json().get("id") is not None
+    assert response.headers.get("content-type") == "application/json"
+
+    response = requests.get("http://127.0.0.1:8000/user/")
+    assert response.status_code == 200
+    assert len(response.json().get("users")) > 0
+    assert response.headers.get("content-type") == "application/json"
+
+    print("All tests have passed")
